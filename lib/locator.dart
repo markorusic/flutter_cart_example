dd76c302544961434b00e058008a8205096ec3db
Marko Rusic <markousp5@gmail.com>
2019-09-07 15:16:47 +0200

    rm unused code

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,10 +1,8 @@
 import 'package:get_it/get_it.dart';
-import 'package:flutter_cart_example/services/category_service.dart';
 import 'package:flutter_cart_example/services/product_service.dart';
 
 GetIt locator = GetIt.I;
 
 void setupLocator() {
   locator.registerLazySingleton(() => ProductService());
-  locator.registerLazySingleton(() => CategoryService());
 }
